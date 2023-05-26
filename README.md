c98d4fb2affe29965d2ab708bde13a3f045d910b
Florian Enner <florian@hebirobotics.com>
2023-05-26 02:00:50 +0200

    implemented simple annotation processor for graalvm config

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # NativeImageConfigGenerator
-Annotation processor for generating native image config files
+
+AOT compiling Java applications via GraalVM's native image is becoming more and more popular, but there are extra steps needed to enable [Reflection Use](https://www.graalvm.org/22.0/reference-manual/native-image/Reflection/) and work with JNI. The automatic detection via GraalVM's agent works well, but it needs to exhaustively execute all code paths, which gets tedious in projects that change often.
+
+This project is a simple annotation processor that automatically generates GraalVM native-image config files for reflection based dependency injection. For example, the annotated class below
+
+```java
+public class AnnotatedPojo {
+
+    @Inject
+    String someString;
+    
+    @FXML
+    Button button;
+
+    @FXML
+    public void initialize(URL location, ResourceBundle resources) {
+    }
+
+}
+```
+
+produces a file in `META-INF/native-image/annotations/reflect-config-di.json` that contains the required configuration for using reflective DI:
+
+```json
+[{
+    "name": "us.hebi.gui.AnnotatedPojo",
+    "methods": [{
+      "name": "initialize",
+      "parameterTypes": ["java.net.URL", "java.util.ResourceBundle"]
+    }],
+    "fields": [{
+        "name": "button"
+    }, {
+        "name": "someString"
+    }]
+}]
+```
+
+The annotation processor is automatically enabled when added as a dependency. This project was intended to be a quick demo to check how difficult this would be to implement, so for now there are no Maven releases, and it needs to be built manually for testing purposes
+
+```xml
+<dependency>
+    <groupId>us.hebi.graalvm</groupId>
+    <artifactId>annotation-processor</artifactId>
+    <version>1.0-SNAPSHOT</version>
+    <scope>compile</scope>
+</dependency>
+```
---
+++ src/main/java/us/hebi/graalvm/NativeImageAnnotationProcessor.java
@@ -0,0 +1,103 @@
+package us.hebi.graalvm;
+
+import com.google.auto.common.BasicAnnotationProcessor;
+import com.google.auto.service.AutoService;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.ImmutableSetMultimap;
+import us.hebi.graalvm.protos.ClassConfig;
+import us.hebi.quickbuf.JsonSink;
+import us.hebi.quickbuf.RepeatedMessage;
+
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.VariableElement;
+import javax.tools.StandardLocation;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.function.Function;
+
+/**
+ * A simple annotation processor that creates GraalVM native-image
+ * configuration files for annotated fields and methods.
+ *
+ * @author Florian Enner
+ * @since 25 May 2023
+ */
+@AutoService(javax.annotation.processing.Processor.class)
+@SupportedSourceVersion(SourceVersion.RELEASE_17)
+public class NativeImageAnnotationProcessor extends BasicAnnotationProcessor {
+
+    @Override
+    protected Iterable<? extends Step> steps() {
+        return ImmutableSet.of(new DependencyInjectionConfigProcessor());
+    }
+
+    class DependencyInjectionConfigProcessor implements Step {
+
+        private static final String DI_CONFIG_FILE = "META-INF/native-image/annotations/reflect-config-di.json";
+
+        @Override
+        public Set<String> annotations() {
+            return Set.of(
+                    "jakarta.inject.Inject",
+                    "jakarta.annotation.PostConstruct",
+                    "jakarta.annotation.PreDestroy",
+                    "javax.inject.Inject",
+                    "javax.annotation.PreDestroy",
+                    "javax.annotation.PostConstruct",
+                    "javafx.fxml.FXML"
+            );
+        }
+
+        @Override
+        public Set<? extends Element> process(ImmutableSetMultimap<String, Element> elementMap) {
+
+            // Protobuf messages that conform to the GraalVM spec when written as JSON
+            RepeatedMessage<ClassConfig> reflectConfig = RepeatedMessage.newEmptyInstance(ClassConfig.getFactory());
+            HashMap<String, ClassConfig> classMap = new HashMap<>();
+            Function<Element, ClassConfig> getClassConfig = classElement -> classMap.computeIfAbsent(classElement.toString(), name -> reflectConfig.next().setName(name));
+
+            // Add all annotated elements to the config
+            elementMap.values().forEach(element -> {
+                switch (element.getKind()) {
+                    case INTERFACE, ENUM, CLASS, RECORD -> {
+                        getClassConfig.apply(element);
+                    }
+                    case FIELD -> {
+                        getClassConfig.apply(element.getEnclosingElement())
+                                .getMutableFields().next().setName(element.getSimpleName());
+                    }
+                    case CONSTRUCTOR, METHOD -> {
+                        var params = getClassConfig.apply(element.getEnclosingElement())
+                                .getMutableMethods().next().setName(element.getSimpleName())
+                                .getMutableParameterTypes();
+                        for (VariableElement param : ((ExecutableElement) element).getParameters()) {
+                            params.add(param.asType().toString());
+                        }
+                    }
+                }
+            });
+
+            // Write JSON content
+            try {
+                var target = processingEnv.getFiler().createResource(StandardLocation.CLASS_OUTPUT, "", DI_CONFIG_FILE);
+                try (Writer out = target.openWriter()) {
+                    out.write(JsonSink.newPrettyInstance()
+                            .writeRepeatedMessageSilent(reflectConfig)
+                            .toString());
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            return Collections.emptySet();
+        }
+
+    }
+
+}
